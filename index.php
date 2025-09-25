8cd42aebc582abda93f4ccd6d4c6ab8935823efe
Muzmmil207 <mly88207@gmail.com>
2025-09-25 22:38:44 +0300

    Add initial implementation of BenriBot for WooCommerce plugin with admin settings and chat widget integration

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
  * Description: Integrates the BenriBot chat widget into your WooCommerce store.
  * Version:     1.0.0
  * Author:      BenriBot
- * Author URI:  https://benribot.com
  * License:     GPLv2 or later
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: benribot-for-woocommerce
